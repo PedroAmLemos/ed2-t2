8799ecdc2542792d4b4b1aca378f4edcc995e151
PedroAmLemos <peedroamlemos@gmail.com>
2021-11-09 13:43:02 -0300

    removed geoTXT, added a blocks tree and print geosvg complete

--- src/treatment.c
+++ src/treatment.c
@@ -1,56 +1,54 @@
 #include <stdio.h>
 #include <string.h>
+#include "svg.h"
 #include "treatment.h"
+#include "kdtree.h"
+#include "block.h"
 
 
-void main_treatment(FILE *geoFile, FILE *qryFile, FILE *viaFile, FILE *geoSVGFile, FILE *geoTXTFile, FILE *qrySVGFile, FILE *qryTXTFile){
-    double x = 0, y = 0, w = 0, h = 0; 
+void main_treatment(FILE *geoFile, FILE *qryFile, FILE *viaFile, FILE *geoSVGFile, FILE *qrySVGFile, FILE *qryTXTFile){
+    double x = 0, y = 0, w = 0, h = 0, point[2]; 
     char aux[5], cep[200], cfill[200], cstrk[200], sw[200];
+    Block_t block;
+    KDTree_t blocks_tree = create_kd_tree();
+
+
     while(fscanf(geoFile, "%s", aux)!=EOF){
         if((strcmp(aux,"q"))==0) {
-            fscanf(geoFile, "%s %lf %lf %lf %lf", cep, &x, &y, &w, &h);
             // criar e armazenar quadra
+            fscanf(geoFile, "%s %lf %lf %lf %lf", cep, &x, &y, &w, &h);
+            block = create_block(cep, x, y, w, h, sw, cstrk, cfill);
+            point[0] = x; point[1] = y;
+            insert_kd(blocks_tree, block, point);
         }
         if((strcmp(aux, "cq"))==0){
             // atualizar os valores de cores e expessura
             fscanf(geoFile, "%s %s %s", sw, cfill, cstrk);
         }
     }
+    open_svg(geoSVGFile);
+    print_tree(get_kd_root(blocks_tree), geoSVGFile, print_block);
+    close_svg(geoSVGFile);
 
 
-    int nv;
-    char id[200], ldir[200], lesq[200], cmp[200], vm[200], nome[200];
-    double i = 0, j = 0;
-    x = 0, y = 0;
-    fscanf(viaFile, "%s", aux);
-    if(strcmp(aux, "nv")==0) {
-        fscanf(viaFile, "%d", &nv);
-    }else{
-        nv = 50000;
-        rewind(viaFile);
-    }
+    if(viaFile){
+        char id[200], ldir[200], lesq[200], cmp[200], vm[200], nome[200];
+        double i = 0, j = 0;
+        x = 0, y = 0;
+        while(fscanf(viaFile, "%s", aux)!=EOF){
+            if((strcmp(aux, "v"))==0){
+                fscanf(viaFile, "%s %lf %lf", id, &x, &y);
+                // criar e armazenar o vertice;
+            }
+            if((strcmp(aux, "e"))==0){
+                fscanf(viaFile, "%lf %lf %s %s %s %s %s", &i, &j, ldir, lesq, cmp, vm, nome);
+                // criar e armazenar a aresta
 
-    while(fscanf(viaFile, "%s", aux)!=EOF){
-        if((strcmp(aux, "v"))==0){
-            fscanf(viaFile, "%s %lf %lf", id, &x, &y);
-            // criar e armazenar o vertice;
-        }
-        if((strcmp(aux, "e"))==0){
-            fscanf(viaFile, "%lf %lf %s %s %s %s %s", &i, &j, ldir, lesq, cmp, vm, nome);
-            // criar e armazenar a aresta
-
+            }
         }
     }
 
 
 
-
-
-
-
-    
-
-
-
-
+    delete_kd(blocks_tree);
 }

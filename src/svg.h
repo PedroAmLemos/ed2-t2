041be3f99df7ef4ddd641f2b7093f15a940fcf88
PedroAmLemos <peedroamlemos@gmail.com>
2021-11-09 13:42:04 -0300

    added svg files

---
+++ src/svg.h
@@ -0,0 +1,63 @@
+#ifndef SVG_H_
+#define SVG_H_
+#include <stdio.h>
+#include "kdtree.h"
+
+/*
+ * Função que abre a tag svg em um arquivo passado.
+ * Pré: ponteiro para o arquivo svg.
+ * Pós:
+ */
+void open_svg(FILE *svgFile);
+
+/*
+ * Função que fecha a tag svg em um arquivo passado.
+ * Pré: ponteiro para o arquivo svg.
+ * Pós:
+ */
+void close_svg(FILE *svgFile);
+
+/*
+ * Função que imprime as informações de uma árvore em um arquivo passado.
+ * Pré: ponteiro do tipo void* para a árvore, ponteiro para o arquivo svg e uma função que realizará a impressão.
+ * Pós:
+ */
+void print_tree(KDTree_t tree_, FILE *svgFile, void(*print)(void*, FILE*));
+
+
+/*
+ * Imprime uma linha em um arquivo svg.
+ * Pré: os valores para impressão da linha e o arquivo svg.
+ * Pós:
+ */
+void print_line(double x1, double y1, double x2, double y2, char stroke[], FILE *svgFile);
+
+/*
+ * Imprime um texto em um arquivo svg.
+ * Pré: os valores para a impressão do texto e o arquivo svg.
+ * Pós:
+ */
+void print_text(double x, double y, char text[], FILE *svgFile);
+
+/*
+ * Imprime um círculo em um arquivo svg.
+ * Pré: os valores para a impressão do círculo e o arquivo svg.
+ * Pós:
+ */
+void print_circle(double cx, double cy, double r, char stroke[], char fill[], char sw[], FILE *svgFile);
+
+/*
+ * Imprime um retângulo tracejado.
+ * Pré: recebe os valores para impressão e o arquivo svg.
+ * Pós:
+ */
+void print_rectangle_dashed(double x, double y, double w, double h, FILE *svgFile);
+
+/*
+ * Imprime um retângulo.
+ * Pré: recebe os valores para impressão e o arquivo svg.
+ * Pós:
+ */
+void print_rectangle(double x, double y, double w, double h, char *fill, char *stroke, FILE *svgFile);
+
+#endif //SVG_H_

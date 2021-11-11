9ed5d8b26e32f8f85774b3817ea6f11265bc2795
PedroAmLemos <peedroamlemos@gmail.com>
2021-11-11 02:05:34 -0300

    getters and create function

--- src/vertex.c
+++ src/vertex.c
@@ -1,5 +1,36 @@
+#include <string.h>
+#include <stdlib.h>
 #include "vertex.h"
+#include "point.h"
 
-// typedef struct Vertex{
-// 
-// }
+typedef struct Vertex{
+    char id[200];
+    Point_t point;
+}Vertex;
+
+Vertex_t create_vertex(char *name, double x, double y){
+    Vertex *vertex = (Vertex*)malloc(sizeof(Vertex));
+    strcpy(vertex->id, name);
+    vertex->point = create_point(x, y);
+    return vertex;
+}
+
+char *get_vertex_name(Vertex_t _vertex){
+    Vertex *vertex = (Vertex*) _vertex;
+    return vertex->id;
+}
+
+Point_t get_vertex_point(Vertex_t _vertex){
+    Vertex *vertex = (Vertex*) _vertex;
+    return vertex->point;
+}
+
+double get_vertex_x(Vertex_t _vertex){
+    Vertex *vertex = (Vertex*) _vertex;
+    return get_point_x(vertex->point);
+}
+
+double get_vertex_y(Vertex_t _vertex){
+    Vertex *vertex = (Vertex*) _vertex;
+    return get_point_y(vertex->point);
+}
--- src/vertex.h
+++ src/vertex.h
@@ -1,5 +1,19 @@
 #ifndef VERTEX_H_
 #define VERTEX_H_
+#include "point.h"
+
 typedef void* Vertex_t;
+
+char *get_vertex_name(Vertex_t _vertex);
+
+Vertex_t create_vertex(char *name, double x, double y);
+
+char *get_vertex_name(Vertex_t _vertex);
+
+Point_t get_vertex_point(Vertex_t _vertex);
+
+double get_vertex_x(Vertex_t _vertex);
+
+double get_vertex_y(Vertex_t _vertex);
 
 #endif

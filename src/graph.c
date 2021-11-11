9e543ad87a112d990795179c8da7a9778f92dd3d
PedroAmLemos <peedroamlemos@gmail.com>
2021-11-11 02:05:23 -0300

    implemented some graph functions

--- src/graph.c
+++ src/graph.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+#include <string.h>
 #include "graph.h"
+#include "edge.h"
 #include "linked_list.h"
 #include "vertex.h"
 
@@ -12,6 +14,25 @@ Graph_t create_graph(){
     return create_list();
 }
 
+Vertex_t get_graph_vertex(AdjList_t _adj){
+    AdjList *al = (AdjList*)_adj;
+    return al->start;
+}
+
+List_t get_graph_edges(AdjList_t _adj){
+    AdjList *al = (AdjList*)_adj;
+    return al->edges;
+}
+
+AdjList_t get_graph_adj_list(Graph_t _graph, char* vertex_name){
+    for(ListNode_t node = get_list_first(_graph); node != NULL; node = get_list_next(node)){
+        AdjList* al = get_list_info(node);
+        if(strcmp(vertex_name, get_vertex_name(al->start)) == 0){
+            return al;
+        }
+    }
+    return NULL;
+}
 
 void add_graph_vertex(Graph_t _graph, Vertex_t _vertex){
     AdjList *adj = (AdjList *) malloc(sizeof(AdjList));
@@ -20,10 +41,32 @@ void add_graph_vertex(Graph_t _graph, Vertex_t _vertex){
     insert_list(_graph, adj);
 }
 
+void add_graph_edge(Graph_t _graph, Edge_t _edge){
+    AdjList *al = get_graph_adj_list(_graph, get_edge_begin_vertex_name(_edge));
+    if(al == NULL){
+        return;
+    }
+    insert_list(al->edges, _edge);
+}
 
-// void adicionaVertice(Graph graph, Vertice vertice){
-//     AdjascentListStruct* als = (AdjascentListStruct*)malloc(sizeof(AdjascentListStruct));
-//     als->inicio = vertice;
-//     als->arestas = create();
-//     insert(graph, als);
-// }
+void delete_adj_list(AdjList_t _adj){
+    AdjList *al = (AdjList*) _adj;
+    // TODO see this
+    remove_list(al->edges, free);
+    free(get_vertex_point(al->start));
+    free(al->start);
+    free(_adj);
+}
+
+void delete_graph(Graph_t _graph){
+    remove_list(_graph, NULL);
+}
+
+
+
+
+
+
+
+
+

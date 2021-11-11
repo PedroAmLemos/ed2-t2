2681f2c06ad6c44448af8d06e8d9101b46fb289c
PedroAmLemos <peedroamlemos@gmail.com>
2021-11-11 12:09:34 -0300

    refactors

--- src/edge.h
+++ src/edge.h
@@ -3,7 +3,7 @@
 
 typedef void* Edge_t;
 
-Edge_t createAresta(char* name, char* begin_vertex_name, char* end_vertex_name, char* ldir, char* lesq, double cmp, double vm);
+Edge_t create_edge(char* name, char* begin_vertex_name, char* end_vertex_name, char* ldir, char* lesq, double cmp, double vm);
 
 char *get_edge_name(Edge_t _edge);
 

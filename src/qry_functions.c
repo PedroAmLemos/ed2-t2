03830660c47165005a6d8bbde5f9fa2a02f88b0c
PedroAmLemos <peedroamlemos@gmail.com>
2021-11-16 14:21:53 -0300

    changed catac to not remove the vertexes and remove all edges

--- src/qry_functions.c
+++ src/qry_functions.c
@@ -68,11 +68,12 @@ void catac(City_t _city, double x, double y, double w, double h, FILE *qrySVGFil
 
 
     // remover quadras
+    double *key;
     List_t points_inside = get_info_inside_kd(blocks_tree, x, y, w, h);
     for(ListNode_t aux = get_list_first(points_inside); aux != NULL; aux = get_list_next(aux)){
         if(is_block_inside_rect(get_list_info(aux), x, y, w, h)){
             block = get_list_info(aux);
-            double *key = get_block_point(block);
+            key = get_block_point(block);
             fprintf(qryTXTFile, "Quadra removida ==> CEP: %s PONTO: x = %.2lf, y = %.2lf LARGURA = %.2lf, ALTURA = %.2lf ESPESSURA DA BORDA: "
                         "%s, COR DE PREENCHIMENTO %s, COR DA BORDA: %s\n", get_block_cep(block), get_block_x(block), get_block_y(block), get_block_width(block),
                         get_block_height(block), get_block_sw(block), get_block_fill(block), get_block_stroke(block));
@@ -83,21 +84,42 @@ void catac(City_t _city, double x, double y, double w, double h, FILE *qrySVGFil
     remove_list(points_inside, NULL);
 
 
-    // remover vertices 
+    // remover vertices e suas arestas
     List_t vertex_inside = get_info_inside_kd(vertex_tree, x, y, w, h);
     AdjList_t adj_list = NULL;
     Vertex_t vertex = NULL;
     char vertex_name[200];
-    ListInfo_t graph_node = NULL;
     List_t edge_list = NULL;
     Edge_t edge = NULL;
     Point_t point = NULL;
+
     for(ListNode_t aux = get_list_first(vertex_inside); aux != NULL; aux = get_list_next(aux)){
         vertex = get_list_info(aux);
         point = get_vertex_point(vertex);
         if(is_pont_inside_rect(point, x, y, w, h)){
-            fprintf(qryTXTFile, "Vertice removido ==> ID: %s X: %f Y: %f\n", get_vertex_name(vertex), get_vertex_x(vertex), get_vertex_y(vertex));
             strcpy(vertex_name, get_vertex_name(vertex));
+            ListNode_t edge_list_node = NULL;
+            edge_list = NULL;
+            edge = NULL;
+            char *ending_vertex_name = NULL;
+            for(ListNode_t node = get_list_first(graph); node != NULL; node = get_list_next(node)){
+                adj_list = get_list_info(node);
+                edge_list = get_graph_edges(adj_list);
+                edge_list_node = get_list_first(edge_list);
+                while(edge_list_node != NULL){
+                    edge = get_list_info(edge_list_node);
+                    ending_vertex_name = get_edge_end_vertex_name(edge);
+                    if(strcmp(ending_vertex_name, vertex_name)==0){
+                        ListNode_t aux = get_list_next(edge_list_node);
+                        fprintf(qryTXTFile, "Aresta removida ==> NOME: %s NOME VERTICE INICIAL: %s NOME VERTICE FINAL: %s LESQ: %s LDIR: %s CMP: %f VM: %f\n", get_edge_name(edge), 
+                                get_edge_begin_vertex_name(edge), get_edge_end_vertex_name(edge), get_edge_lesq(edge), get_edge_ldir(edge), get_edge_cmp(edge), get_edge_vm(edge));
+                        remove_list_node(edge_list, edge_list_node, free);
+                        edge_list_node = aux;
+                        continue;
+                    }
+                    edge_list_node = get_list_next(edge_list_node);
+                }
+            }
             adj_list = get_graph_adj_list(graph, vertex_name);
             edge_list = get_graph_edges(adj_list);
             for(ListNode_t edge_node = get_list_first(edge_list); edge_node != NULL; edge_node = get_list_next(edge_node)){
@@ -105,40 +127,10 @@ void catac(City_t _city, double x, double y, double w, double h, FILE *qrySVGFil
                 fprintf(qryTXTFile, "Aresta removida ==> NOME: %s NOME VERTICE INICIAL: %s NOME VERTICE FINAL: %s LESQ: %s LDIR: %s CMP: %f VM: %f\n", get_edge_name(edge), 
                         get_edge_begin_vertex_name(edge), get_edge_end_vertex_name(edge), get_edge_lesq(edge), get_edge_ldir(edge), get_edge_cmp(edge), get_edge_vm(edge));
             }
-            graph_node = get_graph_node(graph, vertex_name);
-            remove_list_node(graph, graph_node, NULL);
-            delete_adj_list(adj_list);
+            clean_adj_edges(adj_list);
         }
     }
     remove_list(vertex_inside, NULL);
-
-    // remover arestas capengas
-    ListNode_t edge_list_node = NULL;
-    edge_list = NULL;
-    edge = NULL;
-    char *ending_vertex_name = NULL;
-
-    for(ListNode_t node = get_list_first(graph); node != NULL; node = get_list_next(node)){
-        adj_list = get_list_info(node);
-        edge_list = get_graph_edges(adj_list);
-        edge_list_node = get_list_first(edge_list);
-        while(edge_list_node != NULL){
-            edge = get_list_info(edge_list_node);
-            ending_vertex_name = get_edge_end_vertex_name(edge);
-            if(strcmp(ending_vertex_name, vertex_name)==0){
-                ListNode_t aux = get_list_next(edge_list_node);
-                fprintf(qryTXTFile, "Aresta removida ==> NOME: %s NOME VERTICE INICIAL: %s NOME VERTICE FINAL: %s LESQ: %s LDIR: %s CMP: %f VM: %f\n", get_edge_name(edge), 
-                        get_edge_begin_vertex_name(edge), get_edge_end_vertex_name(edge), get_edge_lesq(edge), get_edge_ldir(edge), get_edge_cmp(edge), get_edge_vm(edge));
-                remove_list_node(edge_list, edge_list_node, free);
-                edge_list_node = aux;
-                printf("ARESTA REMOVIDA\n");
-                continue;
-            }
-            edge_list_node = get_list_next(edge_list_node);
-        }
-    }
-
-    //TODO remover todas as arestas que começam / terminal no vertice removido, as que começam ja estão sendo removidas. e escrever no txt.
 }
 Graph_t rv(Graph_t graph, double x, double y, double w, double h, double f){
     Graph_t rv_Graph = create_graph_copy(graph);

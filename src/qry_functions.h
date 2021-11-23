2f8c088f61043aae6ec206bb8a2e1cd89d061a80
PedroAmLemos <peedroamlemos@gmail.com>
2021-11-23 17:44:48 -0300

    mst done, rv now does not return

--- src/qry_functions.h
+++ src/qry_functions.h
@@ -7,6 +7,6 @@ Point_t arroba_o_int(City_t _city, char *cep, char face, int num, FILE *svgFile)
 
 void catac(City_t _city, double x, double y, double w, double h, FILE *qrySVGFile, FILE *qryTXTFile);
 
-Graph_t rv(City_t _city, double x, double y, double w, double h, double f);
+void rv(City_t _city, double x, double y, double w, double h, double f, FILE *qrySVGFile, FILE* qryTXTFile);
 
 #endif

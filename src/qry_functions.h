6031f3f2d430f0969a70c9b3e77844850181bb3a
pedroamlemos <peedroamlemos@gmail.com>
2021-12-04 14:08:00 -0300

    added @p and dijkstra

--- src/qry_functions.h
+++ src/qry_functions.h
@@ -3,12 +3,14 @@
 #include "city.h"
 
 
-Point_t arroba_o_int(City_t _city, char *cep, char face, int num, FILE *svgFile);
+Point_t arroba_o_int(City_t _city, char *cep, char face, int num, FILE *svgFile, int flag);
 
 void catac(City_t _city, double x, double y, double w, double h, FILE *qrySVGFile, FILE *qryTXTFile);
 
 void rv(City_t _city, double x, double y, double w, double h, double f, FILE *qrySVGFile, FILE* qryTXTFile);
 
 void cx(City_t city, double limiar, FILE *qrySVGFile, FILE *qryTXTFile);
+
+void p_i(City_t _city, Point_t start_point, char *cep, char face, int num, FILE *svgFile, FILE *txtFile);
 
 #endif

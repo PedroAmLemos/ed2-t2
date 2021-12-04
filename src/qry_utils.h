f108b9092494030f0e27fd6e7ef72dfa8e808529
pedroamlemos <peedroamlemos@gmail.com>
2021-12-04 14:08:11 -0300

    get closest vertex to point

--- src/qry_utils.h
+++ src/qry_utils.h
@@ -1,7 +1,12 @@
 #ifndef QRY_UTILS_H_
 #define QRY_UTILS_H_
+#include "graph.h"
 #include "linked_list.h"
+#include "vertex.h"
 
 int is_name_in_list(List_t _list, char *name);
 
+Vertex_t get_closest_vertex(Graph_t _graph, Point_t point);
+
 #endif
+

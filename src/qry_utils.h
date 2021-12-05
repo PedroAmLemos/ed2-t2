9dacb33518041036ebd82081511b905f9b9d7f63
PedroAmLemos <peedroamlemos@gmail.com>
2021-12-05 19:34:14 -0300

    utility function to create a dijkstra path graph

--- src/qry_utils.h
+++ src/qry_utils.h
@@ -8,5 +8,7 @@ int is_name_in_list(List_t _list, char *name);
 
 Vertex_t get_closest_vertex(Graph_t _graph, Point_t point);
 
+Graph_t create_dijkstra_graph(Graph_t _graph, List_t path);
+
 #endif
 
